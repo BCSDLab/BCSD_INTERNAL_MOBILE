8caee0eee9416bb34353baa5e498e93bdbf15ae6
WooJin Kong <kongwoojin03@gmail.com>
2025-07-05 22:21:59 +0900

    add: Add backhandler dependency

--- build-logic/convention/src/main/java/com/bcsdlab/internal/convention/ComposeProject.kt
+++ build-logic/convention/src/main/java/com/bcsdlab/internal/convention/ComposeProject.kt
@@ -13,6 +13,7 @@ internal fun Project.configureComposeProject(
                 implementation(libs.findLibrary("compose-runtime").get())
                 implementation(libs.findLibrary("compose-material3").get())
                 implementation(libs.findLibrary("compose-ui").get())
+                implementation(libs.findLibrary("compose-ui-backhandler").get())
                 implementation(libs.findLibrary("compose-components-resources").get())
                 implementation(libs.findLibrary("compose-components-ui-tooling-preview").get())
             }
