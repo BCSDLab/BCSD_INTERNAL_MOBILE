8a493c0b1d124ad9cb9fe6c5e98e8bbb2a0e9d29
WooJin Kong <kongwoojin03@gmail.com>
2025-07-05 12:45:18 +0900

    fix: Add IgnoreKtlint annotation

--- composeApp/src/iosMain/kotlin/com/bcsdlab/internal/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/bcsdlab/internal/MainViewController.kt
@@ -1,8 +1,10 @@
 package com.bcsdlab.internal
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.bcsdlab.internal.core.IgnoreKtlint
 import org.koin.compose.KoinApplication
 
+@IgnoreKtlint
 fun MainViewController() = ComposeUIViewController {
     KoinApplication(
         application = internalAppDeclaration(),
---
+++ core/src/commonMain/kotlin/com/bcsdlab/internal/core/IgnoreKtlint.kt
@@ -0,0 +1,3 @@
+package com.bcsdlab.internal.core
+
+annotation class IgnoreKtlint
