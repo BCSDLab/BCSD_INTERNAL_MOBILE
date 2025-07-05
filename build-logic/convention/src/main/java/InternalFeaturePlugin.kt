b3bc4952f4f034701b92ebc92eb65ec10aa9c14f
WooJin Kong <kongwoojin03@gmail.com>
2025-07-05 12:37:32 +0900

    add: Add ktlint

--- build-logic/convention/src/main/java/InternalFeaturePlugin.kt
+++ build-logic/convention/src/main/java/InternalFeaturePlugin.kt
@@ -17,6 +17,7 @@ class InternalFeaturePlugin : Plugin<Project> {
                 apply(libs.findPlugin("composeCompiler").get().get().pluginId)
                 apply(libs.findPlugin("composeHotReload").get().get().pluginId)
                 apply(libs.findPlugin("kotlinx-serialization").get().get().pluginId)
+                apply(libs.findPlugin("ktlint").get().get().pluginId)
             }
 
             extensions.configure<LibraryExtension> {
